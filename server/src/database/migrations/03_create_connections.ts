69a019ae1ce4022b016edf4e510b5a71cfef8546
Diego Dario <diegodario88@gmail.com>
2020-08-08 22:00:52 -0300

    added routes/controllers

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -3,14 +3,17 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('connections', (table) => {
         table.increments('id').primary();
+
         table.integer('user_id')
             .notNullable()
             .references('id')
             .inTable('users')
             .onUpdate('CASCADE')
             .onDelete('CASCADE');
+
         table.timestamp('created_at')
-            .defaultTo('now()');
+            .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+            .notNullable();
     });
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,73 +1,21 @@
 import express, { response } from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
-interface scheduleItem {
-    week_day: number,
-    from: string,
-    to: string
-}
 
 const routes = express.Router();
+
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/', (request, response) => {
     return response.json({ welcomeMessage: 'Welcome to ProffyAPI!! ' })
 });
 
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        });
-
-        const class_id = insertedClassesIds[0];
-
-        const classSchedule = schedule.map((classScheduleItem: scheduleItem) => {
-            return {
-                class_id,
-                week_day: classScheduleItem.week_day,
-                from: convertHourToMinutes(classScheduleItem.from),
-                to: convertHourToMinutes(classScheduleItem.to),
-            }
-        })
-
-
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-
-    } catch (err) {
-        await trx.rollback();
-
-        return response.status(400).json(
-            { error: 'Unexpected error while creating new class' }
-        );
-    }
-
-    return response.status(201).send();
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
